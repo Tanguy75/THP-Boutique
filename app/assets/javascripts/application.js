688f4a6acce5b19343e4f83f78350e4126f8468b
Paul.Graton <paulgtn@hotmail.fr>
2019-03-04 13:32:28 +0100

    Framework Installed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+//= require core/jquery.3.2.1.min
+//= require core/popper.min
+//= require core/bootstrap.min
+//= require plugins/moment.min
+//= require plugins/bootstrap-switch
+//= require plugins/bootstrap-tagsinput
+//= require plugins/bootstrap-selectpicker
+//= require plugins/jasny-bootstrap.min
+//= require plugins/nouislider.min
+//= require plugins/bootstrap-datetimepicker.min
+//= require now-ui-kit
+
