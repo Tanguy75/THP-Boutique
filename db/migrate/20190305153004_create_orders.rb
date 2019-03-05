d30b70572aa86e120bd7a3c22379d426b23e1f62
Paul.Graton <paulgtn@hotmail.fr>
2019-03-05 20:36:55 +0100

    Huge Debug

--- db/migrate/20190305153004_create_orders.rb
+++ db/migrate/20190305153004_create_orders.rb
@@ -1,9 +1,9 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.string :delivery_address
-      t.string :delivery_type
-      t.string :payment_type
+      # t.string :delivery_address
+      # t.string :delivery_type
+      # t.string :payment_type
       t.references :user, foreign_key: true
       t.references :cart, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -40,32 +40,25 @@ ActiveRecord::Schema.define(version: 2019_03_05_153004) do
     t.index ["item_id"], name: "index_join_cart_items_on_item_id"
   end
 
-<<<<<<< HEAD
   create_table "order_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "delivery_address"
-    t.string "delivery_type"
-    t.string "payment_type"
     t.bigint "user_id"
     t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
-=======
-  create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
->>>>>>> developpement
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
