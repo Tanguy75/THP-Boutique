994cc4e89178ab764dd0240e2dcd4188190e670b
Steph Ravo <hsravo1@gmail.com>
2019-03-05 17:07:33 +0100

    modif seed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,4 @@
 class Item < ApplicationRecord
-    
-    validates :price, presence: true
-    validates :price, presence: true, numericality: { greater_than: 0 }
-	validates :image_url, presence: true
 
 	has_many :join_cart_items
     has_many :carts, through: :join_cart_items
