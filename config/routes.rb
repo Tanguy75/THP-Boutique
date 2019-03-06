4db8fea7f98c299f3b3cfa9e86d115641aaaceb9
jaymile <djemil.aminou@gmail.com>
2019-03-06 14:55:23 +0100

    creation du payement avec strip en attente de quelque infos comme le prix et les boutton pour les connecter

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
 
+	resources :charges, only: [:new, :create]
 	resources :items
 	resources :users
 	resources :sessions, only: [:new, :create, :destroy]
