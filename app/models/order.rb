bc387ca441a64d9b5e6d801b02bd8068406a9cf9
okjacky <ookjacky@gmail.com>
2019-03-07 17:57:31 +0100

    mailer admin + order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,19 @@
 class Order < ApplicationRecord
   after_create :orderconfirm_send
   
-  belongs_to :user
+  
   has_many :cart
 
   belongs_to :user
 	#belongs_to :cart
 	has_many :order_items
-  has_many :items, through: :order_items
+  has_many :items, through: :order_item
   
   private
 
   def orderconfirm_send
-    UserMailer.orderconfirm_email(self).deliver_now
+    OrderMailer.orderconfirm_email(self).deliver_now
+    OrderMailer.orderadmin_email(self).deliver_now
   end
 
 
