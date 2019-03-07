9b70861f44c8a107d6e2484e1949fe711c40f316
Steph Ravo <hsravo1@gmail.com>
2019-03-07 10:08:18 +0100

    first change profile

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_03_06_131102) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.decimal "price"
+    t.float "price"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
